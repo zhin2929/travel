23433be124e2ec1e9542bfdca46a17e166ccf8de
zhin <zhin2929@qq.com>
2018-04-15 22:38:09 +0800

    列表与字母联动

--- src/pages/city/components/List.vue
+++ src/pages/city/components/List.vue
@@ -18,7 +18,7 @@
         </div>
       </div>
 
-      <div class="area" v-for="(item,key) of cities" :key="key">
+      <div class="area" v-for="(item,key) of cities" :key="key" :ref="key">
         <div class="title border-topbottom">{{key}}</div>
         <div class="item-list">
           <div class="item border-bottom"
@@ -38,10 +38,19 @@ export default {
   name: 'CityList',
   props: {
     cities: Object,
-    hotCities: Array
+    hotCities: Array,
+    letter: String
   },
   mounted () {
     this.scroll = new Bscroll(this.$refs.wrapper)
+  },
+  watch: {
+    letter () {
+      if (this.letter) {
+        const element = this.$refs[this.letter][0]
+        this.scroll.scrollToElement(element)
+      }
+    }
   }
 }
 </script>
