3c814f715c291d329e8a47721985a1c9acf7d74d
zhin <zhin2929@qq.com>
2018-04-15 12:49:15 +0800

    完成header

---
+++ src/pages/home/components/Header.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="header">
+    <div class="header-left">
+      <div class="iconfont back-icon">&#xe624;</div>
+    </div>
+    <div class="header-input">
+      <span class="iconfont">&#xe632;</span>
+      输入城市/景点/游玩主题
+    </div>
+    <div class="header-right">
+      城市
+      <span class="iconfont arrow-icon">&#xe6aa;</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeHeader'
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/varibles.styl'
+.header {
+  display flex
+  line-height: 0.86rem;
+  background: $bgColor;
+  color: #ffffff;
+
+  .header-left {
+    width: 0.64rem;
+    float: left;
+    .back-icon {
+      text-align center
+      font-size .4rem
+    }
+  }
+
+  .header-input {
+    flex: 1;
+    height: 0.64rem;
+    line-height: 0.64rem;
+    margin-top: 0.12rem;
+    margin-left: 0.2rem;
+    padding-left: 0.2rem;
+    background: #ffffff;
+    border-radius: 0.1rem;
+    color #cccccc
+  }
+
+  .header-right {
+    width: 1.24rem;
+    float: right;
+    text-align: center;
+    .arrow-icon {
+      margin-left -.04rem
+      font-size .24rem
+    }
+  }
+}
+</style>
