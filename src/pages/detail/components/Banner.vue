e488567923447f5173be7493b2b522742c25a9ac
zhin <zhin2929@qq.com>
2018-04-16 20:37:06 +0800

    添加动画

--- src/pages/detail/components/Banner.vue
+++ src/pages/detail/components/Banner.vue
@@ -12,12 +12,19 @@
         </div>
       </div>
     </div>
-    <common-gallary @close="handleGallaryClose" :imgs="bannerImgs" v-show="showGallary"></common-gallary>
+    <fade-animation>
+      <common-gallary
+        :imgs="bannerImgs"
+        v-show="showGallary"
+        @close="handleGallaryClose"
+      ></common-gallary>
+    </fade-animation>
   </div>
 </template>
 
 <script>
 import CommonGallary from 'common/gallary/Gallary'
+import FadeAnimation from 'common/fade/FadeAnimation'
 export default {
   name: 'DetailBanner',
   props: {
@@ -39,40 +46,52 @@ export default {
     }
   },
   components: {
-    CommonGallary
+    CommonGallary,
+    FadeAnimation
   }
 }
 </script>
 
 <style lang="stylus" scoped>
-.banner
-    position: relative
-    overflow: hidden
-    height: 0
-    padding-bottom: 55%
-    .banner-img
-      width: 100%
-    .banner-info
-      display: flex
-      position: absolute
-      left: 0
-      right: 0
-      bottom: 0
-      line-height: .6rem
-      color: #fff
-      background-image: linear-gradient(top, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.8))
-      .banner-tittle
-        flex: 1
-        font-size: .32rem
-        padding: 0 .2rem
-      .banner-number
-        height: .32rem
-        line-height: .32rem
-        margin-top: .14rem
-        padding: 0 .4rem
-        border-radius: .2rem
-        background: rgba(0, 0, 0, .8)
-        font-size: .24rem
-        .banner-icon
-          font-size: .24rem
+.banner {
+  position: relative;
+  overflow: hidden;
+  height: 0;
+  padding-bottom: 55%;
+
+  .banner-img {
+    width: 100%;
+  }
+
+  .banner-info {
+    display: flex;
+    position: absolute;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    line-height: 0.6rem;
+    color: #fff;
+    background-image: linear-gradient(top, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.8));
+
+    .banner-tittle {
+      flex: 1;
+      font-size: 0.32rem;
+      padding: 0 0.2rem;
+    }
+
+    .banner-number {
+      height: 0.32rem;
+      line-height: 0.32rem;
+      margin-top: 0.14rem;
+      padding: 0 0.4rem;
+      border-radius: 0.2rem;
+      background: rgba(0, 0, 0, 0.8);
+      font-size: 0.24rem;
+
+      .banner-icon {
+        font-size: 0.24rem;
+      }
+    }
+  }
+}
 </style>
